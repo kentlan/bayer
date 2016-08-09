4014ca4df7eec992011b4b0066b5ec917c8e3482
kentlan <salsicom@gmail.com>
2016-08-09 18:32:48 +0300

    uploaded 5/6

---
+++ code/js/app.slide.s6_1.js
@@ -0,0 +1,88 @@
+	document.addEventListener('presentationInit', function(){
+		var slide = app.slide.s6_1 = {
+			elements: {
+      		painContent: "#s6_1_2"
+    		},
+			onEnter:function(slideElement){
+				app.menu.show(); 
+				util.addClass(slide.element.painContent, 'active');
+				presentetion.menuTop("Mir");	
+				presentetion.menuTopSelected("menu_top_1");
+				presentetion.bgStyle('rv3', 'rv3_', 12);
+				presentetion.prev('mir', 'mir_1', "s6_1");
+				//submitSlideEnter('s1_1', '1', 1, '1', 'A WH_Beauty_1_cycle_2015');
+			},
+			onExit:function(slideElement){
+				submitSlideExit('s6_1');
+				
+	
+			}
+		}
+
+	});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*document.addEventListener('presentationInit', function() {
+													   
+  var slide = app.slide.s1 = {
+	  
+  	elements: {
+      painContent: "#s1_2"
+    },
+    onEnter: function(ele) {
+      util.addClass(slide.element.painContent,'active');
+		document.getElementById('mainmenu').style.cssText="top:-50px;";
+    },
+    onExit: function(ele) {
+      util.removeClass(slide.element.painContent,'active');
+	  
+    }
+  };
+
+
+}); 
+*/
+/*var link1;
+link1 = prompt(link1);
+link1 = "s" + link1;
+setTimeout(function() { app.slideshow.scrollTo(link1) }, 500);*/
